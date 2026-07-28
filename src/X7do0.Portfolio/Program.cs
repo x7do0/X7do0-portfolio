c30c46d791d7629c2d957bd14019021712a40acd
x7do0 <x7do02@gmail.com>
2026-07-28 11:57:26 +0300

    perf: enable compression and baseline security headers

--- src/X7do0.Portfolio/Program.cs
+++ src/X7do0.Portfolio/Program.cs
@@ -7,6 +7,11 @@ builder.Services
     .AddRazorComponents()
     .AddInteractiveServerComponents();
 
+builder.Services.AddResponseCompression(options =>
+{
+    options.EnableForHttps = true;
+});
+
 builder.Services.AddScoped<LanguageState>();
 builder.Services.AddSingleton<PortfolioContentService>();
 
@@ -17,7 +22,19 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+app.UseResponseCompression();
 app.UseHttpsRedirection();
+
+app.Use(async (context, next) =>
+{
+    context.Response.Headers["X-Content-Type-Options"] = "nosniff";
+    context.Response.Headers["X-Frame-Options"] = "DENY";
+    context.Response.Headers["Referrer-Policy"] = "strict-origin-when-cross-origin";
+    context.Response.Headers["Permissions-Policy"] = "camera=(), microphone=(), geolocation=()";
+
+    await next();
+});
+
 app.UseStaticFiles();
 app.UseAntiforgery();
 
