4a1c6332b11c96eb63dbb28dc36456c312a80669
x7do0 <x7do02@gmail.com>
2026-07-28 13:41:38 +0300

    feat: generate robots and sitemap endpoints

--- src/X7do0.Portfolio/Program.cs
+++ src/X7do0.Portfolio/Program.cs
@@ -1,3 +1,5 @@
+using System.Security;
+using System.Text;
 using X7do0.Portfolio.Components;
 using X7do0.Portfolio.Services;
 
@@ -37,6 +39,28 @@ app.Use(async (context, next) =>
 
 app.UseStaticFiles();
 app.UseAntiforgery();
+
+app.MapGet("/robots.txt", (HttpRequest request) =>
+{
+    var origin = $"{request.Scheme}://{request.Host}";
+    var body = $"User-agent: *\nAllow: /\nSitemap: {origin}/sitemap.xml\n";
+    return Results.Text(body, "text/plain", Encoding.UTF8);
+});
+
+app.MapGet("/sitemap.xml", async (HttpRequest request, PortfolioContentService contentService) =>
+{
+    var content = await contentService.LoadAsync("ar");
+    var origin = $"{request.Scheme}://{request.Host}";
+    var paths = new List<string> { "/", "/resume" };
+    paths.AddRange(content.Projects.Select(project => $"/projects/{project.Slug}"));
+
+    var urls = string.Join(
+        Environment.NewLine,
+        paths.Select(path => $"  <url><loc>{SecurityElement.Escape(origin + path)}</loc></url>"));
+
+    var xml = $"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n{urls}\n</urlset>";
+    return Results.Text(xml, "application/xml", Encoding.UTF8);
+});
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
